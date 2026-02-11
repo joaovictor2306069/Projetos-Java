0d1e223e729f008b30ae4e84c57002e06ae0142c
Joao Victor Torrezan <jvtorrezan18@gmail.com>
2026-02-11 11:00:45 -0300

    Add files via upload

---
+++ Projetos Java/Wrapper.java
@@ -0,0 +1,18 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        Byte b = 100;
+        Short s = 1000; 
+        Integer i = 10000;
+        Long l = 100000L;
+        Float f = 3.14f;
+        Double d = 3.14159;
+        Character c = 'A';  
+        Boolean bool = true;
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Integer value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+    }
+}
